7722315cff0db1c47bb6d652827de62ea25f6df6
Anton Boyko <me@boykoant.pro>
2018-08-12 09:37:01 +0300

    cleanup

--- speaker-app/src/app/authentication.service.ts
+++ speaker-app/src/app/authentication.service.ts
@@ -2,28 +2,22 @@ import { Injectable } from '@angular/core';
 
 import { UserAgentApplication } from 'msal';
 
+import { ConfigService } from "./config.service";
+
 @Injectable({
-  providedIn: 'root'
+  	providedIn: 'root'
 })
 
 export class AuthenticationService {
 
-	private applicationConfig: any = {
-		clientID: '5804c7d2-876f-49f4-9874-ebe2496fbfbb',
-		authority: 'https://login.microsoftonline.com/tfp/AzureDayConference.onmicrosoft.com/B2C_1_SignUp_SignIn_Default',
-		b2cScopes: ['https://AzureDayConference.onmicrosoft.com/user.read'],
-		redirectUrl: 'http://localhost:4200'
-	};
-
-	private app: any;
+	private app: UserAgentApplication;
 	public isAuthenticated: boolean = false;
 
 	constructor() {
-		this.app = new UserAgentApplication(this.applicationConfig.clientID, this.applicationConfig.authority,
+		this.app = new UserAgentApplication(ConfigService.AzureAD.clientID, ConfigService.AzureAD.authority,
 			(errorDesc, token, error, tokenType) => {
 				this.isAuthenticated = true;
-			}, { redirectUri: this.applicationConfig.redirectUrl });
-		this.app.redirectUri = this.applicationConfig.redirectUrl;
+			}, { redirectUri: ConfigService.AzureAD.redirectUrl });
 	}
 
 	private getToken(): string {
@@ -40,14 +34,11 @@ export class AuthenticationService {
 	public login() {
 		this.checkAuthentication();
 		if (!this.isAuthenticated) {
-			let tokenData = '';
-			this.app.loginRedirect(this.applicationConfig.b2cScopes)
-				.then(data => { tokenData = data; });
+			this.app.loginRedirect(ConfigService.AzureAD.b2cScopes);
 		}
 	}
 
 	public logout() {
 		this.app.logout();
 	}
-
 }
